2f5f74b9a37da6fe09b44593a58b8a0c6690ed85
Aayush-Rajagopalan <61064137+Aayush-Rajagopalan@users.noreply.github.com>
2022-03-13 21:06:38 +0300

    .

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Privacy Policy Of Via</h1> <br></br>
+        <p className='App-text'>Here's the privacy policy and terms that you agree to when adding the <b>Via Bot</b> to your server, or by being a member of a server that has the bot. This doesn't replace the <a className='App-link' href='https://discordapp.com/developers/docs/legal'><b>Developer Terms of Service</b></a> or <a className='App-link' href='https://discordapp.com/terms'><b> Terms of Service</b></a>.</p>
+        <h2>Bots, Services, and Servers</h2>
+        <p className='App-text'>The following is a list of bots to which this privacy policy applies.</p>
+        <ul className='App-text'>
+          <li><b>Via#0835</b> (851470585313230849)</li>
+          <li><b>Via Alpha#6076</b> (920310412765954048)</li>
+          <li><b>Via Development#7353</b> (874192006707052545)</li>
+          <li><b>Aurora™#7671</b> (833537233817436260)</li>
+          <li><b>Clyde#5791</b> (819545506019409941)</li>
+        </ul>
+        <p className='App-text'>This privacy policy also applies to all of these sites and their subdomains:</p>
+        <ul className='App-text'>
+          <li><b>inex.ga</b></li>
+          <li><b>fly.ahmedbaari.gq</b></li>
+          <li><b>berry.is-a.dev</b></li>
+        </ul>
+
+        <h2>Terminology</h2>
+        <p className='App-text'>In this document, we have used the following terms:</p>
+        <ul className='App-text'>
+          <li><b>Bot:</b>The Via Bot</li>
+          <li><b>Server:</b>A Discord Server</li>
+          <li><b>User:</b>A Discord user</li>
+          <li><b>Server Manager:</b>Anyone who has the ability to add a bot to a server or configure the bot for the server. This is usually an administrator or moderator</li>
+          <li><b>Service Member:</b>Anyone who is a member of server to which one of the bots has been added</li>
+          <li><b>Service User:</b>Anyone who authorizes an application (logs in) for a scope that provides additional information</li>
+        </ul>
+
+        <h2>Data Collected By Command</h2>
+        <p className='App-text'>The following items may be collected and stored when a user runs a command to the bot.</p>
+
+        <h2>Data Collected With Optional Features</h2>
+        <p className='App-text'>The following items may be collected and stored when a user enables optional features. Data will not be collected unless the corresponding feature is enabled.</p>
+        
+        <ul className='App-text'>
+          <li>Logs of chat messages (intercom feature)</li>
+          <li>List of a member's roles (splash roles feature)</li>
+          <li>Server's name, icon, member count, presence count and invite link (partnerships feature)</li>
+          <li>Username and User ID (Music queues feature)</li>
+        </ul>
+
+        <h2>Data Collected Automatically</h2>
+        <p className='App-text'>These are necessary information for the bot to function properly. They are stored only in the cache and are deleted automatically. They are not stored in the database.</p>
+
+        <ul className='App-text'>
+          <li>Date and Time of last activity (For Command Cooldowns)</li>
+          <li>Any data needed for standard operation of Discord bots, such as server permissions, user permissions, and user roles.</li>
+        </ul>
+
+        <h2>Data Collected via Authorization</h2>
+        <p className='App-text'>We do not collect or store any information about the user who logs in.</p>
+
+        <h2>Data Storage</h2>
+        <p className='App-text'>We store all of the data collected in the cache. This is the only place where data is stored. We do not store any data in the database. The bot runs on a password protected platform, thus keeping your data safe. Please keep in mind that even with these protections, no data can ever be 100% secure. We do everything to keep your data secure and private, but its absolute security cannot be guaranteed.</p>
+      
+      
+        <h2>Agreement</h2>
+        <p className='App-text'>By adding any of Via bot to your server or using these bots or services in any way, you are consenting to the policies outlined in this document. In addition, you (the server manager) are agreeing to inform your members of the Developer Terms of Service and the contents of this document. If you, the server manager, do not agree to this document, you may remove the bot(s) from the server. If you, the server member, do not agree to this document, you may leave the server that contains the bot(s).</p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;

4b3189f899b539939ff65dc467bafe6fd00e6428
Ahmed Baari <ahmed4baari@gmail.com>
2022-03-15 12:22:29 +0530

    added analytics, fbchat, ico, title, desc, cover

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@
 import './App.css';
 import JsonDataDisplay from './table';
 import { useEffect } from 'react';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
 let options = {
   method: 'GET'
 };
+
+
+Sentry.init({
+  dsn: "https://d4689097fdef46be943ca9eed1ba1754@o1166757.ingest.sentry.io/6257300",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 function App() {
   const makeAPICall = async () => {
